78475606ce8743571dc076ef576c518c21503ff1
Jordan Pratt <jpratt@mtroyal.ca>
2023-01-09 21:20:05 +0000

    Pushed my stuff.

---
+++ htdocs/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>LEC-01</title>
+</head>
+
+<body>
+  <h1><?= "Hello, World!" ?></h1>
+  <ol>
+    <li class="large">foo</li>
+    <li class="large">foo</li>
+    <li class="large">foo</li>
+  </ol>
+</body>
+
+</html>
